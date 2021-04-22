f00847e10db0cf900aa78938a317ac464217bc42
Alejandro Parra <alejandro_parra_15@hotmail.com>
2021-04-21 21:56:14 -0400

    feat(Project): General mockup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Explanation from './components/Explanation';
+import Form from './components/Form';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_container d-flex justify-content-center align-items-center">
+      <div className="container d-flex flex-column justify-content-center">
+        <Explanation />
+        <Form />
+      </div>
     </div>
   );
 }
---
+++ src/components/Explanation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Explanation = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+};
+
+export default Explanation;
